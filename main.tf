5e2da0d6646f24cd70f38a694a12b373843b041e
Rahul Singh <rstraining4@gmail.com>
2022-01-31 19:44:49 +0000

    k8s

--- main.tf
+++ main.tf
@@ -11,25 +11,25 @@ resource "aws_instance" "k8s" {
 
  
 user_data = <<-EOF
-               #!/bin/bash
-                yum install -y httpd php git wget
-                cd /var/www/html
-                wget https://github.com/rstraining4/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/cat1.jpg
-                mv cat1.jpg cat.jpg
-                wget https://github.com/rstraining4/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/index.php
-                mv /var/www/html/htaccess /var/www/html/.htaccess
-                mkdir /var/www/html/cat
-                cp /var/www/html/cat.jpg /var/www/html/cat
-                cp /var/www/html/index.php /var/www/html/cat
-                cp /var/www/html/.htaccess /var/www/html/cat
-                sudo systemctl start httpd
-                sudo systemctl enable httpd
-                sudo usermod -a -G apache ec2-user
-                sudo chown -R ec2-user:apache /var/www
-                sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
-                find /var/www -type f -exec sudo chmod 0664 {} \;
+                #!/bin/bash
+                curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+                cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+                deb http://apt.kubernetes.io/ kubernetes-xenial main
                 EOF
-  tags = {
+                apt-get update -y
+                apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+                rm -rf /var/lib/kubelet/*
+                sysctl net.bridge.bridge-nf-call-iptables=1
+                cat >  /etc/docker/daemon.json << EOF 
+                {
+                "exec-opts": ["native.cgroupdriver=systemd"]
+                }
+                EOF
+                systemctl daemon-reload
+                systemctl restart docker
+                swapoff -a
+                EOF
+    tags = {
     Name = "k8s-${count.index}"
   }
   }
