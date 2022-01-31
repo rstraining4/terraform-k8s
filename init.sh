e9345af8086509a4f53eaf74cf2c0ae2db0b24c1
Rahul Singh <rstraining4@gmail.com>
2022-01-31 19:51:19 +0000

    k8s

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+                curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+                cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+                deb http://apt.kubernetes.io/ kubernetes-xenial main
+                EOF
+                apt-get update -y
+                apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+                rm -rf /var/lib/kubelet/*
+                sysctl net.bridge.bridge-nf-call-iptables=1
+                cat >  /etc/docker/daemon.json << EOF 
+                {
+                "exec-opts": ["native.cgroupdriver=systemd"]
+                }
+                EOF
+                systemctl daemon-reload
+                systemctl restart docker
+                swapoff -a
+                EOF
--- main.tf
+++ main.tf
@@ -9,27 +9,9 @@ resource "aws_instance" "k8s" {
   associate_public_ip_address = true
   count                  = 3
 
- 
-user_data = <<-EOF
-                #!/bin/bash
-                curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-                cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-                deb http://apt.kubernetes.io/ kubernetes-xenial main
-                EOF
-                apt-get update -y
-                apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
-                rm -rf /var/lib/kubelet/*
-                sysctl net.bridge.bridge-nf-call-iptables=1
-                cat >  /etc/docker/daemon.json << EOF 
-                {
-                "exec-opts": ["native.cgroupdriver=systemd"]
-                }
-                EOF
-                systemctl daemon-reload
-                systemctl restart docker
-                swapoff -a
-                EOF
-    tags = {
+  user_data = "${file("init.sh")}"
+  
+  tags = {
     Name = "k8s-${count.index}"
   }
   }
