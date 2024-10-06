dd74e772a91a6f11dd0addcfca64dd6d1a4eb94e
Rahul <Rahul@RAHUL>
2024-10-06 15:20:52 +0100

    k8s

--- variable.tf
+++ variable.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
   #replace the region as suits for your requirement
 }
 variable "aws_ami" {
-  default     = "ami-08b1636362efe25e1"
+  default     = "ami-0c8eea98010057bd0"
   description = "Ubuntu"
 }
 
