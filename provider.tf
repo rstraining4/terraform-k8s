2a4f4af6d2f30eb731ed642179db55c9a384225a
Rahul <Rahul@RAHUL>
2024-11-03 10:47:26 +0000

    k8s

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@ provider "aws" {
   #secret_key = "add-your-access_key"
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
-  shared_credentials_file = "$HOME/.aws/credentials"
+  shared_credentials_files = "$HOME/.aws/credentials"
   profile = "default"
 }
